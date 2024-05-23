34be44f7116488bf6f412321c73b844421de745f
Manttus <rbasnyat666@gmail.com>
2024-05-23 11:46:49 +0545

    feat: create query client wrapper

---
+++ app/components/wraps/query_wrapper.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function QueryProviderWrapper({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
+  const client = new QueryClient();
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
