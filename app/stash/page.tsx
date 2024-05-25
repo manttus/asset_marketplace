b3df233793b2be0a6fbc5ff0cde4f7a2687052b9
Manttus <rbasnyat666@gmail.com>
2024-05-25 21:42:48 +0545

    refactor: update ui

--- app/stash/page.tsx
+++ app/stash/page.tsx
@@ -2,14 +2,17 @@
 
 import Card from "../components/common/card/card";
 import Loader from "../components/loading/loader";
+import NoResult from "../components/loading/no_result";
 import { useAccount } from "../feature/store/useAccount";
 import useAsyncMutation from "../hooks/useAsyncMutation";
 import { useAsyncQuery } from "../hooks/useAsyncQuery";
+import useToastify from "../hooks/useToastify";
 import listingAdapter from "./adapter/listing_adapter";
 import stashService from "./services/stash_service";
 
 export default function Home() {
   const { account } = useAccount();
+  const { success, error } = useToastify();
 
   const { data, isLoading, isError } = useAsyncQuery({
     key: ["stash"],
@@ -26,6 +29,12 @@ export default function Home() {
       const response = await stashService();
       return response.listAsset(data.index, data.price);
     },
+    onSuccess: (data) => {
+      success("Asset listed in market");
+    },
+    onError: (_) => {
+      error("Failed to list asset");
+    },
   });
 
   const { mutateAsync: removeMutateAsync } = useAsyncMutation({
@@ -33,6 +42,12 @@ export default function Home() {
     fn: async (data) => {
       const response = await stashService();
       return response.removeList(data.index);
+    },
+    onSuccess: (data) => {
+      success("Asset removed from market");
+    },
+    onError: (_) => {
+      error("Failed to remove asset");
     },
   });
 
@@ -45,13 +60,14 @@ export default function Home() {
   }
 
   return (
-    <main className="flex w-full overflow-y-auto">
-      <div className="flex p-4">
+    <main className="flex w-full h-full overflow-y-auto">
+      {data.length === 0 && <NoResult />}
+      <div className="flex p-4 gap-8">
         {data.map((item: any) => (
           <Card
             key={item.id}
             id={item.id}
-            name={item.name}
+            name={item.title}
             image={item.image}
             isListed={item.isForSale}
             submitHandler={async (data) => mutateAsync(data)}
