f7a3b65261841998db1b658a495ecabd0ef5eab7
Manttus <rbasnyat666@gmail.com>
2024-05-25 14:05:58 +0545

    feat: create data adapter for market contract call

---
+++ app/(marketplace)/adapters/market_adapter.ts
@@ -0,0 +1,17 @@
+function marketAdapter(proxyObject: any): any[] {
+  const target = proxyObject;
+  const resultArray = target.map((tar: any) => {
+    return {
+      id: tar[0],
+      account: tar[1],
+      image: tar[2],
+      category: tar[5],
+      title: tar[3],
+      description: tar[4],
+      price: tar[7],
+    };
+  });
+  return resultArray;
+}
+
+export default marketAdapter;
