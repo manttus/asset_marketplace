15e0ebfd68818f1bbfa636fd7af9256d3cf70bcd
Manttus <rbasnyat666@gmail.com>
2024-05-25 21:38:13 +0545

    refactor: add onerror callback and make callbacks optional

--- app/hooks/useAsyncMutation.ts
+++ app/hooks/useAsyncMutation.ts
@@ -3,17 +3,20 @@ import { useMutation } from "@tanstack/react-query";
 type MutationPayload = {
   key: string[];
   fn: (data: any) => any;
-  onSuccess: (data: any) => void;
+  onSuccess?: (data: any) => void;
+  onError?: (data: any) => void;
 };
 
 export default function useAsyncMutation({
   key,
   fn,
   onSuccess,
+  onError,
 }: MutationPayload) {
   return useMutation({
     mutationFn: fn,
     mutationKey: key,
     onSuccess,
+    onError,
   });
 }
