cef1bb23c403fb1c290ef97a8c4bfff39ce68bdf
Manttus <rbasnyat666@gmail.com>
2024-05-25 09:34:26 +0545

    feat: create new transfer event

---
+++ hardhat/contracts/events/events.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract events {
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+}
