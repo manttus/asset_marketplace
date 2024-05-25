cca3d524a06626485b11917027a0481b76112bd6
Manttus <rbasnyat666@gmail.com>
2024-05-25 23:09:09 +0545

    refactor: update rpc url for localhost

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -6,10 +6,10 @@ const config: HardhatUserConfig = {
   defaultNetwork: "localhost",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8888",
-    },
-    hardnode: {
       url: "http://127.0.0.1:8545",
+    },
+    ganache: {
+      url: "http://127.0.0.1:8888",
     },
   },
 };
