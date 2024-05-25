efd66c5ad70c9d3cec9c714ed33cff3624ed6b42
Manttus <rbasnyat666@gmail.com>
2024-05-25 13:57:03 +0545

    feat: create data adapter for stash contract call

---
+++ app/stash/adapter/listing_adapter.ts
@@ -0,0 +1,19 @@
+function listingAdapter(proxyObject: any): any[] {
+  const target = proxyObject;
+
+  const resultArray = target.map((tar: any) => {
+    return {
+      id: tar[0],
+      account: tar[1],
+      image: tar[2],
+      category: tar[5],
+      title: tar[3],
+      description: tar[4],
+      isForSale: tar[6],
+    };
+  });
+
+  return resultArray;
+}
+
+export default listingAdapter;
