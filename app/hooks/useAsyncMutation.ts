798df5435c11f7500a86771e865ebd997f0e6d4a
Manttus <rbasnyat666@gmail.com>
2024-05-25 22:05:20 +0545

    refactor: add query invalidation

--- app/hooks/useAsyncMutation.ts
+++ app/hooks/useAsyncMutation.ts
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type MutationPayload = {
   key: string[];
   fn: (data: any) => any;
   onSuccess?: (data: any) => void;
   onError?: (data: any) => void;
+  invalidate?: string[];
 };
 
 export default function useAsyncMutation({
@@ -12,11 +13,16 @@ export default function useAsyncMutation({
   fn,
   onSuccess,
   onError,
+  invalidate,
 }: MutationPayload) {
+  const query = useQueryClient();
   return useMutation({
     mutationFn: fn,
     mutationKey: key,
     onSuccess,
     onError,
+    onSettled: () => {
+      query.invalidateQueries(invalidate);
+    },
   });
 }
